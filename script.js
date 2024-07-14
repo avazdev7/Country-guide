95ccc52291fd005126896af864062f2ec52563f2
avazdev7 <avazdev7@gmail.com>
2024-07-14 06:54:51 -0700

    todo list added to home page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 
+
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
     let finaURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
